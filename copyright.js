ed4345bbf090f24039a6f2b8687516f22e97e3df
mouadbimk9 <49323893+mouadbimk9@users.noreply.github.com>
2019-08-02 22:26:14 +0100

    Create copyright.js

---
+++ copyright.js
@@ -0,0 +1,4 @@
+$(document).ready(function () { 
+		if ($("#tukangkredit,.tukangkredit").attr("href") != "http://moadkoora.blogspot.com") {
+        window.location.href = "http://moadkoora.blogspot.com"; }
+});
